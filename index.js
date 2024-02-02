5df4507a817b8abe747a1c732b1bd1c237a6de81
fazlerabbi13 <mohammad0132794@gmail.com>
2024-02-02 21:51:09 +0600

    booking product added and show it on client side from mongodb

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ async function run() {
             const query = {_id: new ObjectId(id)};
             const options = {
                 // Include only the `title` and `imdb` fields in the returned document
-                projection: { title: 1, img: 1,price: 1 },
+                projection: { title: 1, img: 1,price: 1, img: 1 },
               };
             const result = await servicesCollection.findOne(query,options);
             res.send(result);
@@ -53,6 +53,17 @@ async function run() {
 
 
         // bookings
+
+        app.get('/bookings', async(req,res) =>{
+            console.log(req.query.email);
+            let query = {};
+            if(req.query?.email){
+                query = { email: req.query.email }
+            }
+            const result = await bookingCollection.find(query).toArray();
+            res.send(result);
+        })
+
 
         app.post('/bookings', async(req,res) =>{
             const booking = req.body;

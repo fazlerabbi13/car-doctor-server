dffc94e30612b80c18e8c9ce3f47c0ef2e2fd41d
fazlerabbi13 <mohammad0132794@gmail.com>
2024-02-02 16:00:13 +0600

    create a post api.data posted successfully on mongodb by server

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors');
 require('dotenv').config()
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 
@@ -30,17 +30,36 @@ async function run() {
         await client.connect();
 
         const servicesCollection = client.db('carDoctor').collection('services');
-
+        const bookingCollection = client.db('carDoctor').collection('bookings');
+        //services api  
         app.get('/services', async(req,res) =>{
             const cursor = servicesCollection.find();
             const result = await cursor.toArray();
             res.send(result); 
         })
 
+        // specific service checkout
+
+        app.get('/services/:id', async(req,res) =>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const options = {
+                // Include only the `title` and `imdb` fields in the returned document
+                projection: { title: 1, img: 1,price: 1 },
+              };
+            const result = await servicesCollection.findOne(query,options);
+            res.send(result);
+        })
 
 
+        // bookings
 
-
+        app.post('/bookings', async(req,res) =>{
+            const booking = req.body;
+            console.log(booking);
+            const result = await bookingCollection.insertOne(booking);
+            res.send(result)
+        })
 
 
 

c82426836abd36da4438281a435bb760b7f4ed4b
fazlerabbi13 <mohammad0132794@gmail.com>
2024-02-03 14:21:24 +0600

    create delete api delete booking data from database.and create patch api for update status

--- index.js
+++ index.js
@@ -73,6 +73,33 @@ async function run() {
         })
 
 
+        // update
+
+        app.patch('/bookings/:id', async(req,res) =>{
+            const id = req.params.id;
+            const filter = {_id: new ObjectId(id)}
+            const updateBooking = req.body;
+            console.log(updateBooking)
+            const updateDoc = {
+                $set: {
+                  status: updateBooking.status
+                },
+              };
+
+            const result = await bookingCollection.updateOne(filter,updateDoc)
+              res.send(result)
+        })
+
+
+
+        // delete
+
+        app.delete('/bookings/:id',async(req,res) =>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await bookingCollection.deleteOne(query);
+            res.send(result);
+        })
 
 
 
